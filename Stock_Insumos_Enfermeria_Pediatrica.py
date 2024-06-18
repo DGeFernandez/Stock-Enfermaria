a6257666e6a21a008768a1dcbf6ecdba58a6f534
DGeFernandez <geppifernandez@gmail.com>
2024-06-18 16:21:20 -0300

    Add files via upload

---
+++ Stock_Insumos_Enfermeria_Pediatrica.py
@@ -0,0 +1,73 @@
+"""
+Stock de Insumos de Enfermería Pediátrica 
+en un Centro de Atención Comunitaria.-
+Authors: Carolina Nazareno, 
+        Alba Marlene Bareiro Servin,
+        Claudio Alejandro Ortega,
+        Diana Ivón Geppi Fernández.
+Fecha: 2024
+Version: 1.0
+"""
+
+import os
+import colorama # type: ignore # para instalar colorama ( pip install colorama / pip3 install colorama)
+
+def limpiarPantalla():
+    '''
+    Función limpiarPantalla()
+    Authors: Carolina Nazareno, 
+             Alba Marlene Bareiro Servin, 
+             Claudio Alejandro Ortega,
+             Diana Ivón Geppi Fernández.
+    Fecha: 2024
+    Version: 1.0
+    paramatros:
+        no requiere
+    retorno:
+        retorna la opción seleccionada por el usuario
+    '''
+    os.system('cls' if os.name == 'nt' else 'clear') # Función que limpia la pantalla para sistemas operativos Windows y Linux
+    return
+
+def identificarProfesional():
+    '''
+    Función identificarProfesional()
+    Author: Diana Ivón Geppi Fernández.
+    Fecha: 2024
+    Version: 1.0
+    paramatros:
+        no requiere
+    '''
+profesionalValido = input("Ingrese su usuario, por favor: ")
+contraseniaValida = input("Ingrese su contraseña, por favor: ")
+intentos = 1
+
+while intentos <= 3:
+    if profesionalValido == "stock" and contraseniaValida == "1234":
+        print("Bienvenido. Ud. tiene acceso al programa.")
+        break
+    else:
+        print(f"Usuario y contraseña incorrectos. Ingrese nuevamente su usuario y contraseña correcta. Le quedan {3 - intentos} intentos.")
+        profesionalValido = input("Ingrese su usuario, por favor: ")
+        contraseniaValida = input("Ingrese su contraseña, por favor: ")
+        intentos += 1
+
+if intentos > 3:
+    print("Se han terminado los intentos. No accede al programa.")
+
+def menu():
+    limpiarPantalla()
+    print(colorama.Fore.LIGHTGREEN_EX +"Stock de Enfermería Pediatría en Centro de Atención Comunitaria".center(45))
+    print("-"*65)
+    print(colorama.Fore.CYAN +"1.-" + colorama.Fore.RESET + "Para agregar un insumo ")
+    print(colorama.Fore.CYAN +"2.-" + colorama.Fore.RESET + "Para buscar un insumo ")
+    print(colorama.Fore.CYAN +"3.-" + colorama.Fore.RESET + "Para modificar un insumo ")
+    print(colorama.Fore.CYAN +"4.-" + colorama.Fore.RESET + "Para eliminar un insumo ")
+    print(colorama.Fore.CYAN +"5.-" + colorama.Fore.RESET + "Para solicitar un insumo ")
+    print(colorama.Fore.CYAN +"6.-" + colorama.Fore.RESET + "Para listar los insumo ")
+    op=int(input("Seleccione una opción: "))
+    return op
+# Programa Principal
+
+identificarProfesional()
+menu()
