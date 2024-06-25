ba42cc5abb08f5552040ea135a9906fa34f55044
CaroNazareno <caronazareno@gmail.com>
2024-06-25 07:51:38 -0300

    Add files via upload

---
+++ ArticEnfermeria.py
@@ -0,0 +1,54 @@
+inventario_dicc = {
+    "Guantes": ["tamaño S"],
+    "Guantes": ["tamaño M"],
+    "Guantes": ["tamaño G"],
+    "Jeringas": ["10 cm³"],
+    "Jeringas": ["20 cm³"],
+    "Jeringas": ["5 cc"],
+    "Jeringas": ["3 cc"],
+    "Jeringas": ["1 ml"],
+    "Agujas": ["25/8"],
+    "Agujas": ["16/5"],
+    "Agujas": ["40/8"],
+    "Agujas": ["50/8"],
+    "Algodón": "plegado 500gr",
+    "Alcohol etílico o etanol":"70%",
+    "Alcohol etílico o etanol":"90%",
+    "Catéteres intravenosos": "venoso central"
+    "Catéteres intravenosos": "corto",
+    "Llave de 3 vías": "con conector luer"
+    "Llave de 3 vías": "sin conector luer",
+    "Butter flay": "Catéter para venoclisis MARIPOSA VENOFIX G-23 20 mm.",
+    "Prolongadores": "B17",
+    "Prolongadores": "B14",
+    "Sondas vesicales para niños": ["10"],
+    "Sondas vesicales para niños": ["12"],
+    "Sondas vesicales para niños": ["14"],
+    "Sondas naso-gástricas para niños": ["30"],
+    "Sondas naso-gástricas para niños": ["31"],
+    "Máscaras p/nbz":"Flexible PVC",
+    "Máscaras de oxígeno": "Gafas Nasales"
+    "Mascarilla de oxígeno":" Simple"
+    "Mascarilla de oxígeno":"con Reservorio"
+    "Mascarilla de oxígeno": "tipo Venturi",
+    "Cánulas": "Cánulas intravenosas",
+    "Cánulas": "Cánulas nasales"
+    "Guías de bomba":"para desplazamiento positivo",
+    "Guías de bomba":"para lineares peristálticas"
+    "Guías de bomba":"para peristálticas rotativas."
+    "Agua oxigenada": "peróxido de hidrógeno 30%",
+    "Agua oxigenada": "peróxido de hidrógeno 50%"
+    "Agua oxigenada": "peróxido de hidrógeno 9%"
+    "Pervinox solución":"Povidona-Iodo",
+    "Pervinox solución jabonosa": "Betadine Povidona-Iodo ",
+    "Gasas": "Gasa estéril",
+    "Gasas": "Gasa no estéril",
+    "Gasas": "Gasa hidrófila"
+    "Gasas": "Gasa adhesiva"
+    "Gasas": "Gasa de malla"
+    "Apósitos":  "Apósitos Hidrocoloides",
+    "Apósitos":  "Apósitos de Silicona."
+    "Baja lenguas de madera": "Depresor lingual de madera"
+}
+
+print(inventario_dicc)
