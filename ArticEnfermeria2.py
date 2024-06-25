682d63e0376d723b6698c5b9de3f420a1a9f7c12
CaroNazareno <caronazareno@gmail.com>
2024-06-25 12:15:34 -0300

    Add files via upload

---
+++ ArticEnfermeria2.py
@@ -0,0 +1,27 @@
+inventario_dicc = {
+
+    "Guantes": ["tamaño S", "tamaño M", "tamaño G"],
+    "Jeringas": ["10 cm³", "20 cm³", "5 cc", "3 cc", "1 ml"],
+    "Agujas": ["25/8", "16/5", "40/8", "50/8"],
+    "Algodón": "plegado 500gr",
+    "Alcohol etílico o etanol": ["70%", "90%"],
+    "Catéteres intravenosos": ["venoso central", "corto"],
+    "Llave de 3 vías": ["con conector luer", "sin conector luer"],
+    "Butter flay": "Catéter para venoclisis MARIPOSA VENOFIX G-23 20 mm.",
+    "Prolongadores": ["B17", "B14"],
+    "Sondas vesicales para niños": ["10", "12", "14"],
+    "Sondas naso-gástricas para niños": ["30", "31"],
+    "Máscaras p/nbz": "Flexible PVC",
+    "Máscaras de oxígeno": ["Gafas Nasales", "Simple", "con Reservorio", "tipo Venturi"],
+    "Cánulas": ["Cánulas intravenosas", "Cánulas nasales"],
+    "Guías de bomba": ["para desplazamiento positivo", "para lineares peristálticas", "para peristálticas rotativas"],
+    "Agua oxigenada": ["peróxido de hidrógeno 30%", "peróxido de hidrógeno 50%", "peróxido de hidrógeno 9%"],
+    "Pervinox solución": "Povidona-Iodo",
+    "Pervinox solución jabonosa": "Betadine Povidona-Iodo",
+    "Gasas": ["Gasa estéril", "Gasa no estéril", "Gasa hidrófila", "Gasa adhesiva", "Gasa de malla"],
+    "Apósitos": ["Apósitos Hidrocoloides", "Apósitos de Silicona"],
+    "Baja lenguas de madera": "Depresor lingual de madera"
+}
+
+print(inventario_dicc)
+inventario_dicc{ }
